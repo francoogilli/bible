733b4476c636ac109e48bd6324ef3af53287a4c0
Franco Gilli <francogilli10@gmail.com>
2024-04-28 16:26:12 -0300

    Add responsive to images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start px-24 relative">
-      <div className="relative w-[550px] h-[550px] mx-auto">
+    <main className="flex min-h-screen flex-col items-center justify-start  px-4 md:px-24 relative">
+      <div className="relative w-[300px] h-[300px] md:w-[550px] md:h-[550px] ">
         <img src="/laurels.png" className="absolute inset-0 w-full h-full" style={{ animation: "levitate 16s ease infinite" }}/>
         <img src="/book.png" className="absolute inset-0 w-full h-full" style={{ animation: "levitate 13s ease infinite" }}/>
       </div>
