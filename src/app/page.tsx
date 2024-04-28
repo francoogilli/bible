bc4268cefa5e28fa89f4827fbb24839235948fd2
Franco Gilli <francogilli10@gmail.com>
2024-04-28 16:15:18 -0300

    Add images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-6xl font-extrabold text-white">Bible</h1>
+    <main className="flex min-h-screen flex-col items-center justify-start px-24 relative">
+      <div className="relative w-[550px] h-[550px] mx-auto">
+        <img src="/laurels.png" className="absolute inset-0 w-full h-full" style={{ animation: "levitate 16s ease infinite" }}/>
+        <img src="/book.png" className="absolute inset-0 w-full h-full" style={{ animation: "levitate 13s ease infinite" }}/>
+      </div>
+      <h1 className="text-6xl font-extrabold text-white z-10 mt-8">Biblia</h1>
     </main>
   );
 }
