43f43669dd9c3e2ec3c3f193fca55437da9e53d0
Franco Gilli <francogilli10@gmail.com>
2024-05-02 17:59:17 -0300

    Add leaves to the sides of the hero image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,8 @@ export default function Home() {
         <img src="/Laurel-Tiras.png" className="absolute inset-0 w-full h-full" style={{ animation: "levitate 30s ease infinite" }}/>
         <img src="/Book1.png" className="absolute inset-0 w-full h-full" style={{ animation: "levitate 15s ease infinite" }}/>
         <img src="/Cruz.png" className="absolute inset-0 w-full h-full" style={{ animation: "levitate 12s ease infinite" }}/>
+        <img src="/HojaDerecha.png" className="absolute inset-0 w-full h-full" style={{ animation: "levitate 10s ease infinite" }}/>
+        <img src="/HojaIzquierda.png" className="absolute inset-0 w-full h-full" style={{ animation: "levitate 10s ease infinite" }}/>
       </div>
 
       <div className="max-w-3xl text-center">
