a210b5475f0ae24486b4f1cdea9d6b65a18a6216
Franco Gilli <francogilli10@gmail.com>
2024-05-02 17:09:18 -0300

    Improve padding top on mobile devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import GitHub from "@/icons/GitHub";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-start  px-4 md:px-24 relative">
-      <div className="relative w-[300px] h-[300px] md:w-[450px] md:h-[450px] mt-20 md:mt-0">
+      <div className="relative w-[300px] h-[300px] md:w-[450px] md:h-[450px] mt-24 md:mt-0">
         <img src="/Laurel-Tiras.png" className="absolute inset-0 w-full h-full" style={{ animation: "levitate 30s ease infinite" }}/>
         <img src="/Book1.png" className="absolute inset-0 w-full h-full" style={{ animation: "levitate 15s ease infinite" }}/>
         <img src="/Cruz.png" className="absolute inset-0 w-full h-full" style={{ animation: "levitate 12s ease infinite" }}/>
