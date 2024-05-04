dd8fd800e7e09a8fca02d6e5cb320c52c684731d
Franco Gilli <francogilli10@gmail.com>
2024-05-04 17:24:04 -0300

    Refactor type definitions and function parameters

--- src/app/books/[slug]/[chapter]/page.tsx
+++ src/app/books/[slug]/[chapter]/page.tsx
@@ -7,11 +7,22 @@ import {
 } from "@/content/bibleBooks"
 import { bibleApiGet } from "@/services/fetchBibleApi"
 
+type Params = {
+  slug: string;
+  chapter: string;
+}
 
+type Verse = {
+  number: number;
+  text: string;
+}
 
+type Book = {
+  chapters: number;
+}
 
 type Props = {
-  params: { slug: string }
+  params: Params;
 }
 
 export function generateMetadata({ params }: Props): Metadata {
@@ -21,20 +32,11 @@ export function generateMetadata({ params }: Props): Metadata {
   }
 }
 
-interface Verse {
-  number: number
-  text: string
-}
-
-interface Book {
-  chapters: number
-}
-
-export default async function Page({ params }) {
-  const { slug, chapter } = params as { slug: string; chapter: string }
+export default async function Page({ params }: Props) {
+  const { slug, chapter } = params
 
   const chapterContent = await bibleApiGet<{
-    verses: Verse[]
+    verses: Verse[];
   }>(`verses/rvr/${bibleBookAbbrevBySlug[slug]}/${chapter}}`)
 
   if (!chapterContent) {
@@ -51,19 +53,19 @@ export default async function Page({ params }) {
 
   return (
     <section className="relative text-white mx-auto mb-20 mt-5 w-full max-w-screen-xl gap-2 space-y-10  lg:px-0">
-        <div className="flex flex-row overflow-x-auto mx-auto max-w-screen-md border-b border-zinc-700 top-0 left-0 z-20 gap-2 bg-background py-2">
-    {[...Array(bookContent.chapters)].map((_, index) => (
-      <Link
-        key={index}
-        href={`/books/${slug}/${index + 1}`}
-        className={`bg-zinc-800 size-10 flex justify-center font-bold items-center rounded-md px-4  py-2 ${
-          Number(chapter) === index + 1 ? 'bg-white text-black' : ''
-        }`}
-      >
-        {(index + 1).toString().padStart(2, "0")}
-      </Link>
-    ))}
-  </div>
+      <div className="flex flex-row overflow-x-auto mx-auto max-w-screen-md border-b border-zinc-700 top-0 left-0 z-20 gap-2 bg-background py-2">
+        {[...Array(bookContent.chapters)].map((_, index) => (
+          <Link
+            key={index}
+            href={`/books/${slug}/${index + 1}`}
+            className={`bg-zinc-800 size-10 flex justify-center font-bold items-center rounded-md px-4  py-2 ${
+              Number(chapter) === index + 1 ? 'bg-white text-black' : ''
+            }`}
+          >
+            {(index + 1).toString().padStart(2, "0")}
+          </Link>
+        ))}
+      </div>
       <div className="mx-auto space-y-5 lg:max-w-screen-md">
         <h1 className="scroll-m-20 text-4xl font-extrabold flex justify-center tracking-tight lg:text-5xl">{`${bibleBookNameBySlug[slug]}, Capítulo ${chapter}`}</h1>
         <div className="space-y-2">
