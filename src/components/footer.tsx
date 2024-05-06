6bfe137ff610ea979fa35c4afd4ce5f32e6642f6
Franco Gilli <francogilli10@gmail.com>
2024-05-06 19:29:51 -0300

    Change background color

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 export function SiteFooter() {
   return (
-    <footer className="sticky bg-[#1b1918] top-0 z-10 w-full border-t border-zinc-800 bg-background">
+    <footer className="sticky bg-[#09090B] top-0 z-10 w-full border-t border-zinc-800 bg-background">
       <p className="container py-5 text-white text-center text-[15px] leading-loose">
         Hecho por{" "}
         
