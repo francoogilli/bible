3aec309e174cd1c3040dd66f1c2b62f154bb6c46
Franco Gilli <francogilli10@gmail.com>
2024-05-06 21:25:42 -0300

    Add Select dropdown menu to change chapter

--- src/app/books/[slug]/[chapter]/page.tsx
+++ src/app/books/[slug]/[chapter]/page.tsx
@@ -1,11 +1,9 @@
-import { Metadata } from "next"
-import Link from "next/link"
-import { notFound } from "next/navigation"
-import {
-  bibleBookAbbrevBySlug,
-  bibleBookNameBySlug,
-} from "@/content/bibleBooks"
-import { bibleApiGet } from "@/services/fetchBibleApi"
+import { Metadata } from "next";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+import { bibleBookAbbrevBySlug, bibleBookNameBySlug } from "@/content/bibleBooks";
+import { bibleApiGet } from "@/services/fetchBibleApi";
+import SelectDropdown from "@/components/selectDropdown";
 
 type Params = {
   slug: string;
@@ -26,47 +24,55 @@ type Props = {
 }
 
 export function generateMetadata({ params }: Props): Metadata {
-  const { slug } = params
+  const { slug } = params;
   return {
     title: bibleBookNameBySlug[slug],
-  }
+  };
 }
 
 export default async function Page({ params }: Props) {
-  const { slug, chapter } = params
+  const { slug, chapter } = params;
 
   const chapterContent = await bibleApiGet<{
     verses: Verse[];
-  }>(`verses/rvr/${bibleBookAbbrevBySlug[slug]}/${chapter}}`)
+  }>(`verses/rvr/${bibleBookAbbrevBySlug[slug]}/${chapter}`);
 
   if (!chapterContent) {
-    return notFound()
+    return notFound();
   }
 
-  const bookContent = await bibleApiGet<Book>(
-    `books/${bibleBookAbbrevBySlug[slug]}`
-  )
+  const bookContent = await bibleApiGet<Book>(`books/${bibleBookAbbrevBySlug[slug]}`);
 
   if (!bookContent) {
-    return notFound()
+    return notFound();
   }
+
+  const options = [...Array(bookContent.chapters)].map((_, index) => ({
+    bookSlug: slug,
+    chapterNumber: index + 1,
+    // Puedes cambiar la imagen predeterminada si lo deseas
+    image: 'https://via.placeholder.com/150', // Cambia esto por la URL de tu imagen
+  }));
 
   return (
     <section className="relative text-white mx-auto mb-20 mt-5 w-full max-w-screen-xl gap-2 space-y-10  lg:px-0">
-      <div className="flex flex-row overflow-x-auto mx-auto max-w-screen-md border-b border-zinc-700 top-0 left-0 z-20 gap-2 bg-background py-2">
+      {/* <div className="flex flex-row overflow-x-auto mx-auto max-w-screen-md border-b border-zinc-700 top-0 left-0 z-20 gap-2 bg-background py-2">
         {[...Array(bookContent.chapters)].map((_, index) => (
           <Link
-          key={index}
-          href={`/books/${slug}/${index + 1}`}
-          className={`size-10 flex justify-center font-bold items-center rounded-lg px-4 py-2 ${
-            Number(chapter) === index + 1 ? 'bg-gradient-to-br from-[#ffffff] to-[#ffefdd] text-black' : 'bg-zinc-800 text-white'
-          }`}
-        >
-          {(index + 1).toString().padStart(2, "0")}
-        </Link>
-        
+            key={index}
+            href={`/books/${slug}/${index + 1}`}
+            className={`size-10 flex justify-center font-bold items-center rounded-lg px-4 py-2 ${
+              Number(chapter) === index + 1 ? 'bg-gradient-to-br from-[#ffffff] to-[#ffefdd] text-black' : 'bg-zinc-800 text-white'
+            }`}
+          >
+            {(index + 1).toString().padStart(2, "0")}
+          </Link>
         ))}
-      </div>
+        
+      </div> */}
+      <div className="max-w-52 mx-auto">
+          <SelectDropdown options={options} selectedChapter={Number(chapter)} />
+        </div>
       <div className="mx-auto space-y-5 lg:max-w-screen-md">
         <h1 className="scroll-m-20 text-4xl font-black text-[#ffa47c] flex justify-center tracking-tight lg:text-5xl">{`${bibleBookNameBySlug[slug]} : ${chapter}`}</h1>
         <div className="space-y-2">
@@ -85,5 +91,5 @@ export default async function Page({ params }: Props) {
         </div>
       </div>
     </section>
-  )
+  );
 }
---
+++ src/components/selectDropdown.tsx
@@ -0,0 +1,117 @@
+"use client";
+import React, { useState } from 'react';
+
+interface Option {
+  bookSlug: string;
+  chapterNumber: number;
+  image: string;
+}
+
+interface SelectDropdownProps {
+  options: Option[];
+  selectedChapter?: number;
+}
+
+const SelectDropdown: React.FC<SelectDropdownProps> = ({ options, selectedChapter }) => {
+  const [selectedOption, setSelectedOption] = useState<Option | null>(null);
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleOptionSelect = (option: Option) => {
+    setSelectedOption(option);
+    setIsMenuOpen(false);
+    // Redireccionar al hacer clic en la opción seleccionada
+    window.location.href = `/books/${option.bookSlug}/${option.chapterNumber}`;
+  };
+
+  return (
+    <div>
+      
+      <div className="relative mt-2">
+        <button
+          type="button"
+          className="relative w-full font-medium cursor-default rounded-xl text-white bg-zinc-800 py-1.5 pl-3 pr-10 text-left shadow-sm ring-1 ring-inset ring-[#2d2d2d]  sm:text-sm sm:leading-6"
+          aria-haspopup="listbox"
+          aria-expanded={isMenuOpen ? 'true' : 'false'}
+          aria-labelledby="listbox-label"
+          onClick={toggleMenu}
+        >
+          <span className="flex items-center">
+            {selectedOption && (
+              <img src={selectedOption.image} alt="" className="h-5 w-5 flex-shrink-0 rounded-full" />
+            )}
+            <span className="ml-3 block truncate">{selectedOption ? `Capitulo ${selectedOption.chapterNumber}` : 'Elige un capitulo'}</span>
+          </span>
+          <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
+            <svg
+              className={`size-3.5 mr-1.5 transition-transform transform ${
+                isMenuOpen ? 'rotate-180' : ''
+              } text-gray-400`}
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 3a.75.75 0 01.55.24l3.25 3.5a.75.75 0 11-1.1 1.02L10 4.852 7.3 7.76a.75.75 0 01-1.1-1.02l3.25-3.5A.75.75 0 0110 3zm-3.76 9.2a.75.75 0 011.06.04l2.7 2.908 2.7-2.908a.75.75 0 111.1 1.02l-3.25 3.5a.75.75 0 01-1.1 0l-3.25-3.5a.75.75 0 01.04-1.06z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </span>
+        </button>
+
+        {isMenuOpen && (
+          <ul
+            className="absolute z-10 mt-1 max-h-56 w-full overflow-auto border border-[#2d2d2d] rounded-xl text-white bg-[#09090B] py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+            tabIndex={-1}
+            role="listbox"
+            aria-labelledby="listbox-label"
+          >
+            {options.map((option, index) => (
+              <li
+                key={index}
+                className={`${
+                  option.chapterNumber === selectedChapter ? 'bg-zinc-800 rounded-xl text-white' : 'text-white hover:bg-[#1e1e1e] rounded-xl cursor-pointer'
+                } relative cursor-default select-none py-2 pl-3 pr-9`}
+                id={`listbox-option-${index}`}
+                role="option"
+                onClick={() => handleOptionSelect(option)}
+              >
+                <div className="flex items-center">
+                  <img
+                    src={option.image}
+                    alt=""
+                    className="h-5 w-5 flex-shrink-0 rounded-full"
+                  />
+                  <span className="font-normal ml-3 block truncate">{`Capitulo ${option.chapterNumber}`}</span>
+                </div>
+
+                {option.chapterNumber === selectedChapter && (
+                  <span className="text-white absolute inset-y-0 right-0 flex items-center pr-4">
+                    <svg
+                      className="h-5 w-5"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </span>
+                )}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default SelectDropdown;
