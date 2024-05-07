ead35f0324561b6e5cdbe636baa771ab23eca24c
Franco Gilli <francogilli10@gmail.com>
2024-05-07 17:17:31 -0300

    Improve weight on Footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 export function SiteFooter() {
   return (
     <footer className="sticky bg-[#09090B] top-0 z-10 w-full border-t border-zinc-800 bg-background">
-      <p className="container py-5 text-gray-400  text-center text-[15px] leading-loose">
+      <p className="container py-5 text-gray-400 font-medium text-center text-[15px] leading-loose">
         Hecho por{" "}
         
         <Link
