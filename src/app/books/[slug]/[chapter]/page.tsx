0bd1b62591bba63ebe5d046e0bbcd98235e3e600
Franco Gilli <francogilli10@gmail.com>
2024-05-06 21:45:40 -0300

    Change size and colors on read page

--- src/app/books/[slug]/[chapter]/page.tsx
+++ src/app/books/[slug]/[chapter]/page.tsx
@@ -70,15 +70,15 @@ export default async function Page({ params }: Props) {
         ))}
         
       </div> */}
-      <div className="max-w-64 mx-auto">
+      <div className="max-w-screen md:max-w-64 mx-auto">
           <SelectDropdown options={options} selectedChapter={Number(chapter)} />
         </div>
       <div className="mx-auto space-y-5 lg:max-w-screen-md">
-        <h1 className="scroll-m-20 text-4xl font-black text-[#ffa47c] flex justify-center tracking-tight lg:text-5xl">{`${bibleBookNameBySlug[slug]} : ${chapter}`}</h1>
+        <h1 className="scroll-m-20 text-4xl font-black text-[#3ff7b1] flex justify-center tracking-tight lg:text-5xl">{`${bibleBookNameBySlug[slug]} : ${chapter}`}</h1>
         <div className="space-y-2">
           {chapterContent.verses.map((verse) => (
             <div key={verse.number} className="space-x-2">
-              <span className="text-xs font-extrabold text-[#ffa47c]  bg-gradient-to-br from-[#5e5d5dcb] to-[#2d2c2c] py-0.5 px-1.5 rounded-md">
+              <span className="text-xs font-extrabold text-[#7cffc0]  bg-gradient-to-br from-[#5e5d5dcb] to-[#2d2c2c] py-0.5 px-1.5 rounded-md">
                 {verse.number}
               </span>
               <p
