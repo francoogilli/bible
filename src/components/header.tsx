3f6d0ed13d4e01283f4127852080101ae3639ee3
Franco Gilli <francogilli10@gmail.com>
2024-05-11 15:33:17 -0300

    Update links paths

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,7 +8,7 @@ export function SiteHeader() {
     <header className="sticky top-0 z-40 w-full border-b border-[#2d2d2d] backdrop-blur-sm">
       <div className="container px-4 flex max-w-[80rem] mx-auto h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <div className="flex gap-6 md:gap-10">
-          <Link href="https://la-biblia.vercel.app/" target="_blank" rel="noopener" className="justify-center items-center gap-x-2 hidden md:flex">
+          <Link href="/"  rel="noopener" className="justify-center items-center gap-x-2 hidden md:flex">
             <Book className="text-white size-5" />
             <p className="flex items-center font-extrabold space-x-2 text-white">
               Biblia
@@ -20,7 +20,7 @@ export function SiteHeader() {
             </div>
             <Link
               className="flex items-center  space-x-2 font-bold text-[#c9c9c9] hover:text-[#e1e1e1] transition duration-200 "
-              href="https://la-biblia.vercel.app/books"
+              href="/books"
             >
               Libros
             </Link>
