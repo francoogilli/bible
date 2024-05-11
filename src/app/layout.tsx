1d9f64ef4f0dec67f67af7de49c09f645456ac69
Franco Gilli <francogilli10@gmail.com>
2024-05-11 11:34:57 -0300

    Change language in layout metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" >
+    <html lang="es" >
         <head />
         <body
           className="min-h-screen bg-background  antialiased"
